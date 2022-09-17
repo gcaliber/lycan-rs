78fff79c55e6eb0dc0e8b0cf5651db4ed4e71380
Michael Green <mgreen1980@gmail.com>
2022-09-17 09:34:37 -0400

    refactor addon into enum

--- src/addon.rs
+++ src/addon.rs
@@ -3,45 +3,39 @@ use std::{collections::HashMap, hash::Hash};
 use reqwest::header::{USER_AGENT, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 
+// #[serde(skip_serializing)]
+
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Source {
-  Github, Tukui, Gitlab, WowInt
+pub enum AddonKind {
+  GithubRelease,
+  GithubRepo{branch: String},
+  TukuiMain,
+  TukuiAddon, 
+  Gitlab,
+  WowInt,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-  pub struct Addon {
-    id: Option<u32>,
-    project: String,
-    source: Source,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    branch: Option<String>,
-    name: Option<String>,
-    version: Option<String>,
-    dirs: Option<Vec<String>>,
-    #[serde(skip_serializing)]
-    download_url: Option<String>,
-    #[serde(skip_serializing)]
-    filename: Option<String>,
+pub struct Addon {
+  pub id: u32,
+  pub project: String,
+  pub name: Option<String>,
+  pub version: Option<String>,
+  pub dirs: Option<Vec<String>>,
+  pub kind: AddonKind,
 }
 
 const USER_AGENT_CHROME: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
 
-trait GetLatest {
-  fn get_latest(&self) -> anyhow::Result<()>;
-}
-
 impl Addon {
-  pub fn new(project: String, source: Source, branch: Option<String>) -> Self {
+  pub fn new(project: String, kind: AddonKind) -> Self {
     Self { 
       project: project, 
-      source: source,
-      branch: branch,
-      id: None,
+      id: 0,
       name: None,
       version: None,
       dirs: None,
-      download_url: None,
-      filename: None,
+      kind: kind,
     }
   }
 
@@ -73,50 +67,29 @@ impl Addon {
   }
 
   fn latest_url(&self) -> String {
-    match self.source {
-      Source::Github => {
-        match &self.branch {
-          Some(b) => format!("https://api.github.com/repos/{}/commits/{}", self.project, b),
-          None => format!("https://api.github.com/repos/{}/releases/latest", self.project)
-        }
-      },
-      Source::Tukui => {
-        if self.project == "tukui" || self.project == "elvui" {
-          format!("https://www.tukui.org/api.php?ui={}", self.project)
-        } else {
-          String::from("https://www.tukui.org/api.php?addons")
-        }
-      },
-      Source::Gitlab => {
-        let url_encoded_project = self.project.replace("/", "%2F");
-        format!("https://gitlab.com/api/v4/projects/{}/releases", url_encoded_project)
-      }
-      Source::WowInt => {
-        format!("https://api.mmoui.com/v3/game/WOW/filedetails/{}.json", self.project)
-      }
+    match self.kind {
+      AddonKind::GithubRelease => format!("https://api.github.com/repos/{}/releases/latest", self.project),
+      AddonKind::GithubRepo{branch} => format!("https://api.github.com/repos/{}/commits/{}", self.project, branch),
+      AddonKind::TukuiMain => format!("https://www.tukui.org/api.php?ui={}", self.project),
+      AddonKind::TukuiAddon => String::from("https://www.tukui.org/api.php?addons"),
+      AddonKind::Gitlab => format!("https://gitlab.com/api/v4/projects/{}/releases", self.project.replace("/", "%2F")),
+      AddonKind::WowInt => format!("https://api.mmoui.com/v3/game/WOW/filedetails/{}.json", self.project),
     }
   }
 
   fn set_version(&mut self, json: serde_json::Value) {
-    match self.source {
-      Source::Github => {
-        if self.branch == None {
-          let v = json["tag_name"].as_str().unwrap();
-          self.version = Some(String::from(if v != "" {v} else {json["name"].as_str().unwrap()}))
-        } else {
-          self.version = Some(String::from(json["sha"].as_str().unwrap()));
-        }
+    match self.kind {
+      AddonKind::GithubRelease => {
+        let v = json["tag_name"].as_str().unwrap();
+        self.version = Some(String::from(if v != "" {v} else {json["name"].as_str().unwrap()}))
       },
-      Source::Tukui => {
-        self.version = Some(String::from(json["version"].as_str().unwrap()));
-      },
-      Source::Gitlab => {
+      AddonKind::GithubRepo{..} => self.version = Some(String::from(json["sha"].as_str().unwrap())),
+      AddonKind::TukuiMain | AddonKind::TukuiAddon => self.version = Some(String::from(json["version"].as_str().unwrap())),
+      AddonKind::Gitlab => {
         let v = json[0]["tag_name"].as_str().unwrap();
         self.version = Some(String::from(if v != "" {v} else {json[0]["name"].as_str().unwrap()}))
       }
-      Source::WowInt => {
-        self.version = Some(String::from(json[0]["UIversion"].as_str().unwrap()));
-      }
+      AddonKind::WowInt => self.version = Some(String::from(json[0]["UIversion"].as_str().unwrap())),
     }
   }
 
--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,10 @@ use std::fs;
 
 use clap::Parser;
 
-use crate::addon::Addon;
-use crate::addon::Source;
+use crate::addon::{Addon, AddonKind};
 mod addon;
 // https://github.com/Tercioo/Plater-Nameplates
 
-
-// mutually exclusive options
-// #[clap(group = "input")]
-// input_file: Option<String>,
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -43,40 +38,43 @@ struct Cli {
 fn main() {
 	let cli = Cli::parse();
 
-	let addons_json = PathBuf::from(r"/home/mike/projects/lycan/test/addons.json");
-	let installed_addons = match read_addons(&addons_json) {
-		Ok(a) => a,
-		Err(e) => Vec::new(),
-	};
+	test().unwrap()
+
+	// let addons_json = PathBuf::from(r"/home/mike/projects/lycan/test/addons.json");
+	// let installed_addons = match read_addons(&addons_json) {
+	// 	Ok(a) => a,
+	// 	Err(e) => Vec::new(),
+	// };
+
+	
 
 
-	if let Some(urls) = cli.install.as_deref() {
-		let mut addons: Vec<Option<Addon>> = Vec::new();
-		for url in urls {
-			addons.push(addon_from_url(url));
-		}
+	// if let Some(urls) = cli.install.as_deref() {
+	// 	let mut addons: Vec<Option<Addon>> = Vec::new();
+	// 	for url in urls {
+	// 		addons.push(addon_from_url(url));
+	// 	}
 
-		for addon in addons.iter().flatten() {
-			match addon.get_latest() {
-				Ok(_) => continue,
-				Err(e) => println!("{}", e)
-			}
-			// get latest json
-			// add update info from latest json
-			// download file
-			// unpack file
-			// parse addon top level directories
-			// move addon dirs from temp to destination
-			// write installed addons to file
-		}
+	// 	for addon in addons.iter().flatten() {
+	// 		match addon.get_latest() {
+	// 			Ok(_) => continue,
+	// 			Err(e) => println!("{}", e)
+	// 		}
+	// 		// get latest json
+	// 		// add update info from latest json
+	// 		// download file
+	// 		// unpack file
+	// 		// parse addon top level directories
+	// 		// move addon dirs from temp to destination
+	// 		// write installed addons to file
+	// 	}
 
-	}
+	// }
 }
 
 
 fn addon_from_url(url: &String) -> Option<Addon> {
 	// implement lazy_static on the regex's here for speed
-	// let re = Regex::new(r"^(?:https?:\/\/)?(?:www\.)?(?P<domain>.*)\.(?:com|org)\/(?P<rest>.*[^\/\n])").unwrap();
 	let re = Regex::new(r#"^(?:https?://)?(?:www\.)?(?P<domain>.*)\.(?:com|org)/(?P<rest>.*)"#).unwrap();
 	let caps = re.captures(url)?;
 	let domain = caps.name("domain")?.as_str();
@@ -86,24 +84,28 @@ fn addon_from_url(url: &String) -> Option<Addon> {
 			let re = Regex::new(r#"^(?P<project>.+?/.+?)(?:/|$)(?:tree/)?(?P<branch>.+)?"#).unwrap();
 			let caps = re.captures(rest)?;
 			let project = caps.name("project")?.as_str();
-			let branch = match caps.name("branch") {
-				Some(m) => Some(String::from(m.as_str())),
-				None => None
-			};
-			Some(Addon::new(String::from(project), Source::Github, branch))
+			Some(match caps.name("branch") {
+				Some(m) => Addon::new(String::from(project), AddonKind::GithubRepo{branch: String::from(m.as_str())}),
+				None => Addon::new(String::from(project), AddonKind::GithubRelease),
+			})
 		},
-		"gitlab" => Some(Addon::new(String::from(rest), Source::Gitlab, None)),
+		"gitlab" => Some(Addon::new(String::from(rest), AddonKind::Gitlab)),
 		"tukui" => {
-			let re = Regex::new(r#"^(?:download|addons)\.php\?(?:ui|id)=(?P<project>.*)"#).unwrap();
+			let re = Regex::new(r#"^(?P<kind>download|addons)\.php\?(?:ui|id)=(?P<project>.*)"#).unwrap();
 			let caps = re.captures(rest)?;
 			let project = caps.name("project")?.as_str();
-			Some(Addon::new(String::from(project), Source::Tukui, None))
+			let kind = caps.name("kind")?.as_str();
+			match kind {
+				"download" => Some(Addon::new(String::from(project), AddonKind::TukuiMain)),
+				"addons" => Some(Addon::new(String::from(project), AddonKind::TukuiAddon)),
+				&_ => None,
+			}
 		},
 		"wowinterface" => {
 			let re = Regex::new(r#"^downloads/info(?P<project>\d*)-"#).unwrap();
 			let caps = re.captures(rest)?;
 			let project = caps.name("project")?.as_str();
-			Some(Addon::new(String::from(project), Source::WowInt, None))
+			Some(Addon::new(String::from(project), AddonKind::WowInt))
 		},
 		&_ => None
 	}
@@ -124,12 +126,27 @@ fn read_addons(path: &PathBuf) -> anyhow::Result<Vec<Addon>> {
 }
 
 fn test() -> Result<()> {
-	let addons_json = PathBuf::from(r"/home/mike/projects/lycan/test/addons.json");
+	// let addons_json = PathBuf::from(r"/home/mike/projects/lycan/test/addons.json");
 	let test_json = PathBuf::from(r"/home/mike/projects/lycan/test/test.json");
 
-	let addons = read_addons(&addons_json).unwrap();
 
-	write_addons(&addons, &test_json).unwrap();
+	let mut a = Addon::new(String::from("test/addon"), AddonKind::GithubRepo { branch: String::from("master") });
+	a.id = 1;
+	a.name = Some(String::from("addon"));
+	a.version = Some(String::from("v1.0"));
+	a.dirs = Some(vec![String::from("dir1"), String::from("dir2")]);
+
+	let mut b = Addon::new(String::from("test/addon222"), AddonKind::Gitlab);
+	b.id = 1;
+	b.name = Some(String::from("addon222"));
+	b.version = Some(String::from("v1.0222"));
+	b.dirs = Some(vec![String::from("dir1"), String::from("dir2")]);
+
+	let aa = vec![a, b];
+
+	// let addons = read_addons(&addons_json).unwrap();
+
+	write_addons(&aa, &test_json).unwrap();
 
 	Ok(())
 }
